371b928a4f9d2b12155cb934f119df2726defa62
xiang ni <xiang_x_ni@manulife.com>
2022-05-09 17:32:04 +0800

    init

---
+++ typings/declarations.d.ts
@@ -0,0 +1,19 @@
+declare module '*.scss' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '@environment' {
+  const module: any;
+
+  export const environment: any;
+  export default module;
+}
+
+declare module '*.js' {
+  const module: any;
+  export default module;
+}
+
+declare const microStore: any;
+declare const fetchCacheData: any;
