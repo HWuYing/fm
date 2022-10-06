42bf0aa878fc8a925ec025e0d46da88e04bafcb6
xiang ni <xiang_x_ni@manulife.com>
2022-10-06 16:49:25 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,20 @@ const gulp = require('gulp');
 const ts = require('gulp-typescript');
 
 const rootOutDir = '.';
+const targetMapping = { CommonJs: 'es5', ES2015: 'es2015', ESNext: 'es5' };
 // const rootOutDir = '../api/node_modules/@fm/';
 
 function g(packageName, moduleItem, stripInternal) {
   const [module, outDir] = moduleItem;
   return () => {
-    const project = ts.createProject('tsconfig.json', { module });
+    const project = ts.createProject('tsconfig.json', { module, target: targetMapping[module] });
     let source = gulp.src([`university/${packageName}/**/*`, 'typings/**/*']).pipe(project());
     source = stripInternal ? source.dts : source.js;
     return source.pipe(gulp.dest(`${rootOutDir}/${packageName}/${outDir}`));
   }
 }
 
-const moduleMapping = { CommonJs: 'cjs', ES2015: 'esm', 'ESNext': "esm5" };
+const moduleMapping = { CommonJs: 'cjs', ES2015: 'esm', ESNext: "esm5" };
 const packages = ['di', 'shared', 'dynamic-builder', 'csr', 'ssr', 'server'];
 // const packages = ['di','shared', 'csr', 'ssr', 'server'];
 // const packages = ['dynamic-builder'];
